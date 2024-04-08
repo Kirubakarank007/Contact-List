2af1bced5497898efc2b000244d951068ce6f1b5
KIRUBAKARAN K <kirubakaran02002@gmail.com>
2024-04-08 19:31:37 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import CurrencyRates from './CurrencyRates';
+
+function App() {
+  return (
+    <CurrencyRates/>
+  );
+}
+
+export default App;
